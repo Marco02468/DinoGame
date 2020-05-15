05fc8a66e431c82e1d314a54b7939c6c8d909d01
Marco02468 <loesche1@gmx.de>
2020-05-15 20:25:01 +0200

    first pic

--- Renderer.py
+++ Renderer.py
@@ -12,41 +12,56 @@ class Renderer:
 
     def start(self):
         self.generateField()
+        self.generateDino()
         self.show()
 
     def show(self):
-        for m in range(self.y):
-            print(self.grid[m])
+        for m in range(15):
+            s = ""
+            #print("\n")
+            for n in range(20):
+                s += str(self.grid[m][n]) + "\t"
+            print(s),
 
-    def spawn(self):
-        return
+    def spawn(self, sprite):
+        startpointX = 17
+        startpointY = 9
+        for i in range(4):
+            for j in range(3):
+                self.grid[startpointY+i][startpointX+j] = sprite[i][j]
+
 
     def generateDino(self):
         dino     = [[["X"], ["X"], [ 0  ]],
                     [[ 0  ], ["X"], [ 0  ]],
                     [[ 0  ], ["X"], ["X"]],
                     [[ 0  ], ["X"], [ 0  ]]]
-        return dino
+        #print(dino)
+        self.spawn(dino)
 
     def generateDuckedDino(self):
         duckedDino =   [[ [ 0  ], [ 0  ], [ 0  ]],
                         [ [ 0  ], [ 0  ], [ 0  ]],
                         [ ["X"], ["X"], ["X"]],
                         [ [ 0  ], ["X"], [ 0  ]]]
-        return duckedDino
+        #print(duckedDino)
+        self.spawn(duckedDino)
 
     def generateBush(self):
         bush = [[ [0],[ 0  ],[ 0  ] ],
                 [ [0],[ 0  ],[ 0  ] ],
                 [ [0],["X"],["X"] ],
                 [ [0],["X"],["X"] ]]
-        return bush
+        #print(bush)
+        self.spawn(bush)
 
     def generateBird(self):
         bush = [[[0], ["X"], ["X"]],
                 [[0], ["X"], ["X"]],
                 [[0], [ 0 ], [ 0 ]],
                 [[0], [ 0 ], [ 0 ]]]
+        #print(bush)
+        self.spawn(bush)
         return bush
 
     def generateField(self):
