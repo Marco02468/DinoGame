f0189ba96da9504549bf3a49007681b84147175d
sophia598 <64893860+sophia598@users.noreply.github.com>
2020-06-08 22:36:00 +0200

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,163 @@
+import time
+#from LedRenderer import LedRenderer
+
+
+class Game:
+    mode = "presentation"
+    x = 20
+    y = 15
+    grid = [[[0, 0, 0, 0] for x in range(20)] for y in range(15)]
+    # grid[yPos][xPos]
+    score = 0
+    alive = True
+    objects = []
+    # 0=bush ; 1=bird ; (type, position)
+
+    jumpPhase = 0  # default 1; low 2=9 ; medium 3=8 ; high 4=7 ; max 5=6           ##### all minus 1
+    jumps = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 0}
+    jumpPos = {0: 0, 1: 1, 2: 2, 3: 3, 4: 2, 5: 1}
+
+    def start(self):
+        while self.alive:
+            self.drawField()
+            self.nextObstacle()
+            self.animate()
+            controlls = self.getInput()
+            self.move(controlls)
+            self.show()
+            self.checkLiving()
+            self.deleteObstacle()
+            self.score += 1
+            time.sleep(0.5)
+            print("score: ", self.score)
+            self.adapter()
+
+    def drawDino(self, sprite):
+        startpointX = 17
+        startpointY = 9 - self.jumpPos[self.jumpPhase]
+        for i in range(4):
+            for j in range(3):
+                self.grid[startpointY + i][startpointX + j] = sprite[i][j]
+
+    def jump(self):
+        self.jumpPhase = self.jumps[self.jumpPhase]
+        dino = self.generateDino()
+        self.drawDino(dino)
+
+    def getInput(self):
+        if self.mode == "presentation":
+            input = self.ai()
+            return input
+        elif self.mode == "play":
+            print("TODO")
+            # input = getInput() and resolve
+
+    def ai(self):
+        if self.grid[11][16] == [0, 1, 0, 0] or self.jumpPhase > 0:  # jump
+            print("jump now!")
+            return "jump"
+        elif self.grid[10][16] == [0, 1, 1, 0]:  # duck
+            return "duck"
+        elif self.grid[11][16] == [0, 0, 0, 0] and self.grid[10][16] == [0, 0, 0, 0] and self.jumpPhase == 0:
+            print("RUN!")
+            return "run"
+
+    def move(self, input):
+        print("jumpphase", self.jumpPhase)
+        if input == "jump":
+            print("You jump")
+            self.jump()
+        elif input == "duck":
+            print("You duck")
+            self.duck()
+        elif input == "run" and self.jumpPhase == 0:
+            print("You run")
+            dino = self.generateDino()
+            self.drawDino(dino)
+
+    def animate(self):
+        for i in range(len(self.objects)):
+            if self.objects[i][0] == 0:  # falls busch and fresh
+                bush = self.getBush()
+                position = self.objects[i][1]
+                self.drawObstacles(bush, position)
+
+            elif self.objects[i][0] == 1:  # falls bird
+                bird = self.getBird()
+                position = self.objects[i][1]
+                self.drawObstacles(bird, position)
+            self.objects[i][1] = self.objects[i][1] + 1
+
+    def drawObstacles(self, sprite, xPosition):
+        for i in range(3):
+            self.grid[i + 10][xPosition] = sprite[i]
+
+    def deleteObstacle(self):
+        for i in range(len(self.objects) - 1):
+            if self.objects[i][1] == 20:
+                self.objects.pop(i)
+
+    def nextObstacle(self):
+        if self.score % 10 == 0:
+            self.objects.append([0, 0])
+            # print("spawn bush ", self.score)
+            return
+        elif self.score % 5 == 0 and False:
+            self.objects.append([1, 0])
+            # print("spawn bush ", self.score)
+            return
+
+    def show(self):
+        for m in range(15):
+            s = ""
+            for n in range(20):
+                s += str(self.grid[m][n]) + "\t"
+            print(s)
+
+    def checkLiving(self):
+        if self.score == 100:
+            self.alive = False
+
+    def duck(self):
+        print()
+
+    def getBush(self):
+        return [[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]]
+
+    def getBird(self):
+        return [[1, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+    def generateDino(self):
+        dino = [[[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+                [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+                [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0]],
+                [[0, 0, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0]]]
+        # print(dino)
+        return dino
+
+    def generateDuckedDino(self):
+        duckedDino = [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+                      [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+                      [[0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]],
+                      [[0, 0, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0]]]
+        # print(duckedDino)
+        return duckedDino
+
+    def drawField(self):
+        self.grid = [[[0, 0, 0, 0] for x in range(20)] for y in range(15)]
+        for m in range(self.x):
+            self.grid[14][m] = [1, 1, 0, 0]
+            self.grid[13][m] = [1, 1, 0, 0]
+
+    def adapter(self):
+        #l = LedRenderer(15, 20)
+        data = []
+        for i in range(14):
+            for j in range(19):
+                data.append(self.grid[i][j])
+        print(data)
+        l.render(data)
+
+
+x = Game()
+x.start()
